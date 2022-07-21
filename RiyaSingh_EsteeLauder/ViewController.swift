5492f05d7efd82f611ba847abd0fd2136018e68a
Riya <riya.singh45296@gmail.com>
2022-07-21 19:55:03 -0400

    Initial Commit

---
+++ RiyaSingh_EsteeLauder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RiyaSingh_EsteeLauder
+//
+//  Created by scholar on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
